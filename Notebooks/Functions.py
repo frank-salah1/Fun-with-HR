d956e566445288d49f2c7e24e58ffd51612a0d2c
frank-salah1 <66169187+frank-salah1@users.noreply.github.com>
2020-12-05 12:48:53 -0500

    Update

--- Notebooks/Functions.py
+++ Notebooks/Functions.py
@@ -7,10 +7,18 @@ from sklearn.model_selection import cross_validate
 from sklearn import metrics
 import seaborn as sns 
 from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from yellowbrick.cluster import KElbowVisualizer
+from sklearn.cluster import KMeans
 
 def log_reg(X, y,cv=5):
     logreg = LogisticRegression(penalty = 'none')
     crossval = cross_validate(logreg, X,y, return_train_score=True, scoring='accuracy', return_estimator=True, cv = cv)
+    return crossval
+
+def lin_reg(X, y, cv=5):
+    lrreg = LinearRegression()
+    crossval = cross_validate(lrreg, X,y, return_train_score=True, scoring='r2', return_estimator=True, cv=cv)
     return crossval
 
 def accuracy_display(estimator, X, y, label = '' ):
@@ -26,3 +34,21 @@ def accuracy_display(estimator, X, y, label = '' ):
     title = 'Accuracy Score: {0}'.format(score)
     plt.title( title, size = 15);
     plt.show()
+
+def graph_display(X, y, labelX = None, labely = None, title = None, pred = None, c1X = None, c1y = None):
+    plt.scatter(X, y, c=pred, s = 25) 
+    plt.scatter(c1X, c1y, c='black', s=100);
+    plt.xlabel(labelX)
+    plt.ylabel(labely)
+    
+def kmeans_groups(min, max, df, metric = 'calinski_harabasz'):
+    model = KMeans()
+    visualizer = KElbowVisualizer(model, k=(min,max), metric = metric, timings=False)
+    visualizer.fit(df)
+    visualizer.show()
+    
+def kmeans_clust (n_clusters, df):
+    k_means = KMeans(n_clusters= n_clusters)
+    k_means.fit(df)
+    y_hat = k_means.predict(df)
+    return k_means, y_hat
